564e5b5d12a0d3fc071e83d159e87d76fccab48c
anogray <bbncr97@gmail.com>
2021-01-23 19:58:01 +0530

    added all

--- src/App.js
+++ src/App.js
@@ -4,13 +4,27 @@ import {Navbar, NavbarBrand} from 'reactstrap';
 import './App.css';
 
 import Main from "./components/MainComponent"
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
+
+const store = ConfigureStore();
 
 class App extends Component {
   render(){
     return (
+<<<<<<< HEAD
+      <Provider store={store}>
+      <BrowserRouter>
       <div>
        <Main/>
       </div>
+      </BrowserRouter>
+      </Provider>
+=======
+      <div>
+       <Main/>
+      </div>
+>>>>>>> 0986343 (removed eslintcache)
     );
   }
   
--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,27 +1,140 @@
-import React, { Component } from 'react';
+import React, { Component, useState,useEffect } from 'react';
 import { Media } from 'reactstrap';
-
-import {
-    Card, CardImg, CardImgOverlay, CardText, CardBody,
-    CardTitle
-} from 'reactstrap';
+import { Card, CardImg, Button, FormGroup, Input, Form,Row, Col, Label, CardText,CardBody,CardTitle, Breadcrumb, BreadcrumbItem, Modal, ModalHeader, ModalBody } from 'reactstrap';
+import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Link } from 'react-router-dom';
 
 
-    
+// const [isModalOpen, handleModal] = useState(false)
 
-    function renderDishComments(DishComment){
+
+const required = (val) => {console.log(val && val.length,{val}); return val && val.length;}
+const maxLength = (len) => (val) => {/*console.log({len,val});*/ return !(val) || (val.length <= len);}
+const minLength = (len) => (val) => val && (val.length >= len);
+
+    function RenderDishComments(DishComment){
+        const [isModalOpen, handleModal] = useState(false)
+        
+        // useEffect(() => {
+        //     console.log("useEffec being called again")
+        // }, [isModalOpen])
+        
+
+        const handleComment = (e)=>{
+              handleModal(!isModalOpen)
+        }
+        
+        function handleSubmit(values) {
+            console.log('Comment submitted is:' + JSON.stringify(values));
+            alert('Comment submitted is: ' + JSON.stringify(values));
+        }
+
+        const CommentForm=()=>{
+
+         return(    
+            <Modal isOpen={isModalOpen} toggle={()=>handleModal(!isModalOpen)}>
+            <ModalHeader toggle={handleComment}>Submit Comment</ModalHeader>
+            <ModalBody>
+            <LocalForm onSubmit={(values) => handleSubmit(values)}>
+            <Label htmlFor="Rating">Rating</Label>
+            <Row className="form-group">
+                <Col md={12}>
+                <Control.select model=".rating" id="rating" className="form-control"
+                validators={{
+                    required
+                }}>
+                <option></option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+              </Control.select>
+              <Errors
+              className="text-danger"
+              model=".rating"
+              show="touched"
+              messages={{
+                  required: 'Required ',
+              }}/>
+                    
+                </Col>
+            </Row>
+            <Label htmlFor="name">Your Name</Label>
+            <Row className="form-group">
+                <Col md={12}>
+                   <Control.text model=".name" id="name" name="name"
+                        placeholder="Your Name"
+                        className="form-control"
+                        validators={{
+                            required, minLength: minLength(3), maxLength: maxLength(15)
+                        }}
+                         />
+                    <Errors
+                        className="text-danger"
+                        model=".name"
+                        show="touched"
+                        messages={{
+                            required: 'Required ',
+                            minLength: 'Must be greater than 2 characters',
+                            maxLength: 'Must be 15 characters or less'
+                        }}
+                     />
+                </Col>
+            </Row>
+            <Label htmlFor="comment">Comment</Label>
+            <Row className="form-group">
+                <Col md={12}>
+                   <Control.textarea model=".comment" id="comment" name="comment"
+                        className="form-control"
+                        rows="6"
+                        validators={{
+                             maxLength: maxLength(150)
+                        }}
+                         />
+                    <Errors
+                        className="text-danger"
+                        model=".comment"
+                        show="touched"
+                        messages={{
+                            required: 'Required',
+                            minLength: 'Must be greater than 2 characters',
+                            maxLength: 'Must be 100 characters or less'
+                        }}
+                     />
+                </Col>
+            </Row>
+            <Row className="form-group">
+                <Col md={{ size: 10, offset: 0 }}>
+                    <Button type="submit" color="primary">
+                        Send Feedback
+                    </Button>
+                </Col>
+            </Row>
+        </LocalForm>
+                
+            </ModalBody>
+        </Modal>
+            )
+        }
+
+
         if(DishComment!=null){
          console.log({DishComment})
-        let commentsAll = DishComment.comments.map(
-            (cmts)=>(
+         let idx = DishComment.length
+        let commentsAll = DishComment.map(
+            (cmts,index)=>(
                     <div>
                     <CardBody>
                     <CardText>{cmts.comment}</CardText>
                     <CardText>--{cmts.author} {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(cmts.date)))}</CardText>
+                    {index==idx-1?<Button outline onClick={handleComment} ><span className="fa fa-pencil fa-lg"></span> Submit Comment</Button>:null}
                     </CardBody>
                     </div> 
                 )
         )
+
+        
 
         return (
             <div>
@@ -29,11 +142,13 @@ import {
                     <h4>Comments</h4>
                 </div>
                  {commentsAll}
+                 {CommentForm()?CommentForm():"CommentForm()"}
+                 
              </div>)
             }
             
-            else
-            return <div></div>
+            // else
+            // return <div></div>
     }
 
     function renderDish(dish) {
@@ -59,17 +174,32 @@ import {
     }
 
     const DishDetail = (props)=> {
+
         
+
         return (
             <div className="container">
+            <div className="row">
+                    <Breadcrumb>
+
+                        <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>{props.dish.name}</h3>
+                        <hr />
+                    </div>                
+                </div>
             <div className="row">
                     <div className="col-12 col-md-5 m-1">
                       {renderDish(props.dish)}  
                     </div>
 
                     <div className="col-12 col-md-5 m-1">
-                    {renderDishComments(props.dish)}
+                    {RenderDishComments(props.comments)}
                     </div>
+
+                    {}
                       
              </div>
              </div>
