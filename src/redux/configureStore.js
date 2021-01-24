b771b22434b539b0219556a05f56d842ae07b773
anogray <bbncr97@gmail.com>
2021-01-24 18:40:43 +0530

    added multiple reducers

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,6 +1,5 @@
 import {createStore, combineReducers} from 'redux';
-import { Reducer, initialState } from './reducer'
-import  Dishes from './dishes';
+import  {Dishes} from './dishes';
 import { Comments } from './comments';
 import { Promotions } from './promotions';
 import { Leaders } from './leaders';
